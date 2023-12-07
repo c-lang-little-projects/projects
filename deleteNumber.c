95205ca4432cf30b85971eed931f5ef13f3150a6
jamal_aldeen_khalil <66186669+jamal-kh@users.noreply.github.com>
2023-12-07 14:56:59 +0300

    Add files via upload

--- deleteNumber.c
+++ deleteNumber.c
@@ -3,19 +3,27 @@
 int main()
 {
 
-    int array[] = {316,813,319,318,183,515};
-    int Num;
-    int len = 7;
-    printf("write wich number you want delete ");
-    scanf("%d",&Num);
-    for(int i = 0; i < 7; i++) {
-        if(Num == array[i] ) {
-            array[i] = array[i + 1];
+    int array[] = {316,813,319,318,183,515,183};
+    int NumtoDelete , i;
+    int len = sizeof(array) / sizeof(int);
+
+    printf("the array :");
+    for(i = 0 ; i < len ; i++){
+        printf(" %d " , array[i]);
+    }
+
+    printf("\nwrite witch number you want to delete : ");
+    scanf("%d",&NumtoDelete);
+    for(i = 0; i < len ; i++) {
+        if(NumtoDelete == array[i]) {
+            for(int j = i ; j < len ; j++){
+                array[j] = array[j + 1];
+            }
             len = len - 1;
         }
     }
-    for(int i = 0; i < 6; i++) {
-        printf("%d \n",array[i]);
+    for(i = 0; i < len; i++) {
+        printf("%d ",array[i]);
     }
     return 0;
 }
